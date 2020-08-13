1eb07598e8810ed8e230bd6d3dd8bb468d145426
Blaze <sandevabeykoon123@gmail.com>
2020-08-13 23:40:44 +0530

    fiexed embed bug

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ client.on("ready", () => {
 // Help Command
 client.on("message", async message => {
     if (!message.guild) return;
-    if (message.content === "/udaw") {
-        message.reply("hello how are you doing");
+    if (message.content === "/hi wifu") {
+        message.reply(exampleEmbed);
     }
 });
-
-
+const exampleEmbed = new discord.MessageEmbed(
+    .setColor('#0099ff')
+    .setTitle('Some title'));
 //Bot Join Command
 client.on("message", async message => {
     if (!message.guild) return;
@@ -125,51 +126,4 @@ client.on("message", async message => {
     }
 });
 
-const exampleEmbed = {
-    color: 0x0099ff,
-    title: "Some title",
-    url: "https://discord.js.org",
-    author: {
-        name: "Some name",
-        icon_url: "https://i.imgur.com/wSTFkRM.png",
-        url: "https://discord.js.org"
-    },
-    description: "Some description here",
-    thumbnail: {
-        url: "https://i.imgur.com/wSTFkRM.png"
-    },
-    fields: [
-        {
-            name: "Regular field title",
-            value: "Some value here"
-        },
-        {
-            name: "\u200b",
-            value: "\u200b",
-            inline: false
-        },
-        {
-            name: "Inline field title",
-            value: "Some value here",
-            inline: true
-        },
-        {
-            name: "Inline field title",
-            value: "Some value here",
-            inline: true
-        },
-        {
-            name: "Inline field title",
-            value: "Some value here",
-            inline: true
-        }
-    ],
-    image: {
-        url: "https://i.imgur.com/wSTFkRM.png"
-    },
-    timestamp: new Date(),
-    footer: {
-        text: "Some footer text here",
-        icon_url: "https://i.imgur.com/wSTFkRM.png"
-    }
-};
+
