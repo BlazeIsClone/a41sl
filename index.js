51d1245ca20b20b81b51071e1fd73df8d602c703
Blaze <sandevabeykoon123@gmail.com>
2020-08-13 20:45:07 +0530

    added embeds

--- index.js
+++ index.js
@@ -13,8 +13,31 @@ client.on("ready", () => {
 // Help Command
 client.on("message", msg => {
     if (msg.content == "/help") {
-        msg.reply("type - /waren");
+        msg.reply(exampleEmbed);
     }
+});
+const exampleEmbed = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Some title")
+    .setURL("https://discord.js.org/")
+    .setAuthor(
+        "Some name",
+        "https://i.imgur.com/wSTFkRM.png",
+        "https://discord.js.org"
+    )
+    .setDescription("Some description here")
+    .setThumbnail("https://i.imgur.com/wSTFkRM.png")
+    .addFields(
+        { name: "Regular field title", value: "Some value here" },
+        { name: "\u200B", value: "\u200B" },
+        { name: "Inline field title", value: "Some value here", inline: true },
+        { name: "Inline field title", value: "Some value here", inline: true }
+    )
+    .addField("Inline field title", "Some value here", true)
+    .setImage("https://i.imgur.com/wSTFkRM.png")
+    .setTimestamp()
+    .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
+
 });
 
 //Bot Join Command
@@ -29,10 +52,9 @@ client.on("message", async message => {
 
 //Bot Leave Command
 client.on("message", async message => {
-    if (!message.guild) return;
     if (message.content === "/palayan") {
         if (message.member.voice.channel) {
-            connection =  message.member.voice.channel.leave();
+            connection = message.member.voice.channel.leave();
         }
     }
 });
