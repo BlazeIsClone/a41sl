328dde5b054b9761c6a821188f35390fef278245
Blaze <sandevabeykoon123@gmail.com>
2020-08-14 00:27:54 +0530

    help command added functionality

--- index.js
+++ index.js
@@ -15,11 +15,37 @@ client.on("message", async message => {
     if (!message.guild) return;
     if (message.content === "/udaw") {
         message.channel.send({embed: {
-          color: 3447003,
-          description: "A very simple Embed!"
-        }});
+            color: 3447003,
+            author: {
+                  name: client.user.username,
+                  icon_url: client.user.avatarURL
+                },
+            title: "This is an embed",
+            url: "http://google.com",
+            description: "This is a test embed to showcase what they look like and what they can do.",
+            fields: [{
+                    name: "Fields",
+                    value: "They can have different fields with small headlines."
+                  },
+                  {
+                          name: "Masked links",
+                          value: "You can put [masked links](http://google.com) inside of rich embeds."
+                        },
+                  {
+                          name: "Markdown",
+                          value: "You can put all the *usual* **__Markdown__** inside of them."
+                        }
+                ],
+            timestamp: new Date(),
+            footer: {
+                  icon_url: client.user.avatarURL,
+                  text: "© Example"
+                }
+          }
+        });
     }
-});
+    
+
 const attachment = new discord.MessageAttachment(
     "https://i.imgur.com/w3duR07.png"
 );
