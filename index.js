8710a2df7aaf585d2c5357f6b9af846d227c693b
Blaze <sandevabeykoon123@gmail.com>
2020-08-13 23:18:21 +0530

    changed help command

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ client.on("ready", () => {
 
 // Help Command
 client.on("message", msg => {
-    if (msg.content === "/udaw") {
-        chennel.send('still working on')
+    if (msg.content == "/udaw") {
+        message.send('no reply')
     }
 });
 
@@ -123,3 +123,51 @@ client.on("message", async message => {
     }
 });
 
+const exampleEmbed = {
+    color: 0x0099ff,
+    title: "Some title",
+    url: "https://discord.js.org",
+    author: {
+        name: "Some name",
+        icon_url: "https://i.imgur.com/wSTFkRM.png",
+        url: "https://discord.js.org"
+    },
+    description: "Some description here",
+    thumbnail: {
+        url: "https://i.imgur.com/wSTFkRM.png"
+    },
+    fields: [
+        {
+            name: "Regular field title",
+            value: "Some value here"
+        },
+        {
+            name: "\u200b",
+            value: "\u200b",
+            inline: false
+        },
+        {
+            name: "Inline field title",
+            value: "Some value here",
+            inline: true
+        },
+        {
+            name: "Inline field title",
+            value: "Some value here",
+            inline: true
+        },
+        {
+            name: "Inline field title",
+            value: "Some value here",
+            inline: true
+        }
+    ],
+    image: {
+        url: "https://i.imgur.com/wSTFkRM.png"
+    },
+    timestamp: new Date(),
+    footer: {
+        text: "Some footer text here",
+        icon_url: "https://i.imgur.com/wSTFkRM.png"
+    }
+};
