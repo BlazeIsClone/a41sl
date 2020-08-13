7c4998f0d362f621f4f2200c664210a275c4535c
Blaze <sandevabeykoon123@gmail.com>
2020-08-13 21:45:14 +0530

    fixed embeds

--- index.js
+++ index.js
@@ -13,38 +13,9 @@ client.on("ready", () => {
 // Help Command
 client.on("message", msg => {
     if (msg.content == "/udaw") {
-        const exampleEmbed = new MessageEmbed()
+        const exampleEmbed = new discord.MessageEmbed()
             .setColor("#0099ff")
             .setTitle("Some title")
-            .setURL("https://discord.js.org/")
-            .setAuthor(
-                "Some name",
-                "https://i.imgur.com/wSTFkRM.png",
-                "https://discord.js.org"
-            )
-            .setDescription("Some description here")
-            .setThumbnail("https://i.imgur.com/wSTFkRM.png")
-            .addFields(
-                { name: "Regular field title", value: "Some value here" },
-                { name: "\u200B", value: "\u200B" },
-                {
-                    name: "Inline field title",
-                    value: "Some value here",
-                    inline: true
-                },
-                {
-                    name: "Inline field title",
-                    value: "Some value here",
-                    inline: true
-                }
-            )
-            .addField("Inline field title", "Some value here", true)
-            .setImage("https://i.imgur.com/wSTFkRM.png")
-            .setTimestamp()
-            .setFooter(
-                "Some footer text here",
-                "https://i.imgur.com/wSTFkRM.png"
-            );
 
         channel.send(exampleEmbed);
     }
