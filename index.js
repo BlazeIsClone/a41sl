eb1e1c094489db39c309ea761ae1c4ba2bfc76e0
Blaze <sandevabeykoon123@gmail.com>
2020-08-14 00:35:32 +0530

    bug fiexed embed

--- index.js
+++ index.js
@@ -25,24 +25,7 @@ client.on("message", async message => {
                 url: "http://google.com",
                 description:
                     "This is a test embed to showcase what they look like and what they can do.",
-                fields: [
-                    {
-                        name: "Fields",
-                        value:
-                            "They can have different fields with small headlines."
-                    },
-                    {
-                        name: "Masked links",
-                        value:
-                            "You can put [masked links](http://google.com) inside of rich embeds."
-                    },
-                    {
-                        name: "Markdown",
-                        value:
-                            "You can put all the *usual* **__Markdown__** inside of them."
-                    }
-                ],
-                timestamp: new Date(),
+               timestamp: new Date(),
                 footer: {
                     icon_url: client.user.avatarURL,
                     text: "© Example"
