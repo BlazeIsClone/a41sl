302c5b1b942e211ea8100bbf8b60b7a36bed20f9
Blaze <sandevabeykoon123@gmail.com>
2020-08-13 21:10:07 +0530

    removed embeds

--- index.js
+++ index.js
@@ -16,28 +16,6 @@ client.on("message", msg => {
         msg.reply(exampleEmbed);
     }
 });
-const exampleEmbed = new discord.MessageEmbed()
-    .setColor("#0099ff")
-    .setTitle("Some title")
-    .setURL("https://discord.js.org/")
-    .setAuthor(
-        "Some name",
-        "https://i.imgur.com/wSTFkRM.png",
-        "https://discord.js.org"
-    )
-    .setDescription("Some description here")
-    .setThumbnail("https://i.imgur.com/wSTFkRM.png")
-    .addFields(
-        { name: "Regular field title", value: "Some value here" },
-        { name: "\u200B", value: "\u200B" },
-        { name: "Inline field title", value: "Some value here", inline: true },
-        { name: "Inline field title", value: "Some value here", inline: true }
-    )
-    .addField("Inline field title", "Some value here", true)
-    .setImage("https://i.imgur.com/wSTFkRM.png")
-    .setTimestamp()
-    .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
-
 
 //Bot Join Command
 client.on("message", async message => {
