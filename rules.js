feffb34c215167cafda0db22d57b2b5bb31bedaa
Blaze <sandevabeykoon123@gmail.com>
2020-08-14 09:27:13 +0530

    obj changed to arr

--- rules.js
+++ rules.js
@@ -1,44 +1,8 @@
 const rules = {
     bodyRules:
-        "    No blank nicknames." ,
-        "    No inappropriate nicknames.",
-        "    No sexually explicit nicknames." ,
-        "    No offensive nicknames." ,
-        "    No inappropriate profile pictures." ,
-        "    No sexually explicit profile pictures." ,
-        "    No offensive profile pictures." ,
-        "    Moderators reserve the right to change nicknames." ,
-        "    Moderators reserve the right to use their own discretion regardless of any rule." ,
-        "    No exploiting loopholes in the rules (please report them)." ,
-        "    No inviting unofficial bots." ,
-        "    No bugs, exploits, glitches, hacks, bugs, etc." ,
-        "    No questioning the mods." ,
-        "    No @mentioning the mods." ,
-        "    No asking to be granted roles/moderator roles." ,
-        "    Contact the moderators under #request-support for support.",
-        "    No @everyone/@here mentioning without permission." ,
-        "    No @mentioning spam." ,
-        "    No illegal content." ,
-        "    No hacking." ,
-        "    No publishing of personal information (including real names, addresses, emails, passwords, bank account and credit card information, etc.)." ,
-        "    No personal attacks." ,
-        "    No witch hunting." ,
-        "    No harassment." ,
-        "    No sexism." ,
-        "    No racism." ,
-        "    No hate speech." ,
-        "    No religious discussions." ,
-        "    No political discussions." ,
-        "    No spamming." ,
-        "    No overusing emojis." ,
-        "    No overusing reactions." ,
-        "    Moderators reserve the right to delete any post." ,
-        "    No advertisement without permission." ,
-        "    No linking to other servers." ,
-        "    Use the right text channel for the topic you wish to discuss." ,
-        "    No annoying, loud or high pitch noises." ,
-        "    Reduce the amount of background noise, if possible." ,
-        "    Moderators reserve the right to disconnect, mute, deafen, or move members to and from voice channels."
+    [
+           "No blank nicknames.", 
+           " No annoying, loud or high pitch noises. " ]
 };
 
 module.exports = rules;
