b223cd85d5e5c5859b19b61809340cf16aaa9bdc
Blaze <sandevabeykoon123@gmail.com>
2020-08-14 09:33:17 +0530

    fiexed rules

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("message", async message => {
                 },
                 title: "Mn Oyata udaw karanawa saho",
                 url: "http://google.com",
-                description: rules.bodyRules[i],
+                description: rules.bodyRules,
                 timestamp: new Date(),
                 footer: {
                     icon_url: client.user.avatarURL,
