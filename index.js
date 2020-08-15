a41721ee250fb9a0ebd51aaa35abbfb97ce1405f
Blaze <sandevabeykoon123@gmail.com>
2020-08-15 11:48:32 +0530

    token fiexed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const { prefix,token } = require("./config.json");
+const { prefix } = require("./config.json");
+const token = process.env.token;
 const ytdl = require("ytdl-core");
 const rules = require("./rules.js");
 const client = new Discord.Client();
