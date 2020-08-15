39447938fa85247f536bf873758e295fa905ca33
Blaze <sandevabeykoon123@gmail.com>
2020-08-15 11:56:29 +0530

    queue fiexd

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ client.once("disconnect", () => {
     console.log("Disconnect!");
 });
 
+const queue = new Map();
+
 client.on("message", async message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
