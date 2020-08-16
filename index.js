b59b97a27719e2c3c632c9a58a36a77e90e6523f
Blaze <sandevabeykoon123@gmail.com>
2020-08-16 12:10:12 +0530

    apply text fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { prefix } = require("./config.json");
 const { MessageEmbed } = require("discord.js");
-const Canvas = require('canvas');
+const Canvas = require("canvas");
 const token = process.env.token;
 const ytdl = require("ytdl-core");
 const rulesEmbed = require("./rules.js");
@@ -173,52 +173,64 @@ client.on("guildMemberAdd", async member => {
         ch => ch.name === "welcome"
     );
     if (!channel) return;
-        const canvas = Canvas.createCanvas(700, 250);
+    const canvas = Canvas.createCanvas(700, 250);
+    const ctx = canvas.getContext("2d");
+
+    const background = await Canvas.loadImage("./wallpaper.png");
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = "#74037b";
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    // Slightly smaller text placed above the member's display name
+    ctx.font = "28px sans-serif";
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(
+        "Welcome to the server,",
+        canvas.width / 2.5,
+        canvas.height / 3.5
+    );
+    const applyText = (canvas, text) => {
         const ctx = canvas.getContext("2d");
 
-        const background = await Canvas.loadImage("./wallpaper.png");
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        let fontSize = 70;
 
-        ctx.strokeStyle = "#74037b";
-        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+        do {
+            // Assign the font to the context and decrement it so it can be measured again
+            ctx.font = `${(fontSize -= 10)}px sans-serif`;
+            // Compare pixel width of the text to the canvas minus the approximate avatar size
+        } while (ctx.measureText(text).width > canvas.width - 300);
 
-        // Slightly smaller text placed above the member's display name
-        ctx.font = "28px sans-serif";
-        ctx.fillStyle = "#ffffff";
-        ctx.fillText(
-            "Welcome to the server,",
-            canvas.width / 2.5,
-            canvas.height / 3.5
-        );
+        // Return the result to use in the actual canvas
+        return ctx.font;
+    };
 
-        // Add an exclamation point here and below
-        ctx.font = applyText(canvas, `${member.displayName}!`);
-        ctx.fillStyle = "#ffffff";
-        ctx.fillText(
-            `${member.displayName}!`,
-            canvas.width / 2.5,
-            canvas.height / 1.8
-        );
+    // Add an exclamation point here and below
+    ctx.font = applyText(canvas, `${member.displayName}!`);
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(
+        `${member.displayName}!`,
+        canvas.width / 2.5,
+        canvas.height / 1.8
+    );
 
-        ctx.beginPath();
-        ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.clip();
+    ctx.beginPath();
+    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
 
-        const avatar = await Canvas.loadImage(
-            member.user.displayAvatarURL({ format: "jpg" })
-        );
-        ctx.drawImage(avatar, 25, 25, 200, 200);
+    const avatar = await Canvas.loadImage(
+        member.user.displayAvatarURL({ format: "jpg" })
+    );
+    ctx.drawImage(avatar, 25, 25, 200, 200);
 
-        const attachment = new Discord.MessageAttachment(
-            canvas.toBuffer(),
-            "welcome-image.png"
-        );
+    const attachment = new Discord.MessageAttachment(
+        canvas.toBuffer(),
+        "welcome-image.png"
+    );
 
-        channel.send(`Welcome to the server, ${member}!`, attachment);
-    });
-
-
+    channel.send(`Welcome to the server, ${member}!`, attachment);
+});
 const kissRadio = "http://198.178.123.8:8404/;";
 const tnlrocksRadio = "http://live.tnlrn.com:8010/live.mp3";
 const goldRadio = "http://209.133.216.3:7048/;";
