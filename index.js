3c06cf69904dee93e40c6a46009f5facdec981a2
Blaze <sandevabeykoon123@gmail.com>
2020-08-16 11:33:57 +0530

    welcome msg updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const { prefix } = require("./config.json");
-const { MessageEmbed } = require("discord.js") 
+const { MessageEmbed } = require("discord.js");
 const token = process.env.token;
 const ytdl = require("ytdl-core");
 const rulesEmbed = require("./rules.js");
@@ -131,7 +131,9 @@ function play(guild, song) {
         })
         .on("error", error => console.error(error));
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing :arrow_forward: : **${song.title}**`);
+    serverQueue.textChannel.send(
+        `Start playing :arrow_forward: : **${song.title}**`
+    );
 }
 
 // Help Command
@@ -169,11 +171,52 @@ client.on("guildMemberAdd", member => {
     const channel = member.guild.channels.cache.find(
         ch => ch.name === "welcome"
     );
-    // Do nothing if the channel wasn't found on this server
     if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}. Have Fun and stay awsome!`);
-});
+        const canvas = Canvas.createCanvas(700, 250);
+        const ctx = canvas.getContext("2d");
+
+        const background = await Canvas.loadImage("./wallpaper.jpg");
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+        ctx.strokeStyle = "#74037b";
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+        // Slightly smaller text placed above the member's display name
+        ctx.font = "28px sans-serif";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(
+            "Welcome to the server,",
+            canvas.width / 2.5,
+            canvas.height / 3.5
+        );
+
+        // Add an exclamation point here and below
+        ctx.font = applyText(canvas, `${member.displayName}!`);
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(
+            `${member.displayName}!`,
+            canvas.width / 2.5,
+            canvas.height / 1.8
+        );
+
+        ctx.beginPath();
+        ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.clip();
+
+        const avatar = await Canvas.loadImage(
+            member.user.displayAvatarURL({ format: "jpg" })
+        );
+        ctx.drawImage(avatar, 25, 25, 200, 200);
+
+        const attachment = new Discord.MessageAttachment(
+            canvas.toBuffer(),
+            "welcome-image.png"
+        );
+
+        channel.send(`Welcome to the server, ${member}!`, attachment);
+    });
+
 
 const kissRadio = "http://198.178.123.8:8404/;";
 const tnlrocksRadio = "http://live.tnlrn.com:8010/live.mp3";
@@ -229,10 +272,10 @@ client.on("message", async message => {
 });
 
 client.on("message", async message => {
-    if(!message.guild) return;
-    if(message.content === "/stop karapn") {
+    if (!message.guild) return;
+    if (message.content === "/stop karapn") {
         dispatcher = connection.end();
-        console.log('dispatcher stoped')
+        console.log("dispatcher stoped");
     }
-})
+});
 client.login(token);
