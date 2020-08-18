2e491d44f2187dc3cd3b0f533329c52247dfc519
Blaze <sandevabeykoon123@gmail.com>
2020-08-18 22:20:56 +0530

    emmiter fiexed

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const rulesEmbed = require("./rules.js");
 const client = new Discord.Client();
 var dispatcher;
 var connection;
+const emitter = new EventEmitter()
+emitter.setMaxListeners(0)
 
 //Bot status
 client.once("ready", () => {
