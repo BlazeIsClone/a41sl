ce6ace90f5d11256caa320a106bf183dd2d26694
Blaze <sandevabeykoon123@gmail.com>
2020-08-19 19:43:15 +0530

    fixed async function member count

--- index.js
+++ index.js
@@ -23,13 +23,11 @@ client.once("ready", () => {
         }
     });
 });
-client.on("ready", function () {
-    setInterval(function () {
+client.on("ready", async function () {
         var memberCount = guild.members.filter(member => !member.user.bot).size;
         var memberCountChannel = bot.channels.get("745643386588889178");
         memberCountChannel.setName("👤members: " + memberCount);
-    }, MIN_INTERVAL);
-});
+    });
 client.on("message", function (message) {
     if (message.content === "!Live") {
         fetch(
