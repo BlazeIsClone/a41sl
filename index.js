14f4534358487ac538f97142d163e29cce78daa5
Blaze <sandevabeykoon123@gmail.com>
2020-08-23 21:41:41 +0530

    gold fm bug fixed

--- index.js
+++ index.js
@@ -34,135 +34,6 @@ client.once("ready", () => {
     });
 });
 
-client.on("message", function (message) {
-    if (message.content === "!Live") {
-        fetch("", function (err, res) {
-            if (res.stream == null) {
-                //mybot.reply(message, "currently not live");
-                console.log("currently not live");
-            } else {
-                //mybot.reply(message, "currently live");
-                console.log("currently live");
-            }
-        });
-    }
-});
-
-/*
-  const queue = new Map();
-
-client.on("message", async message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(PREFIX)) return;
-
-    const serverQueue = queue.get(message.guild.id);
-
-    if (message.content.startsWith(`${PREFIX}play`)) {
-        execute(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${PREFIX}skip`)) {
-        skip(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${PREFIX}stop`)) {
-        stop(message, serverQueue);
-        return;
-    } else {
-        message.channel.send("You need to enter a valid command!");
-    }
-});
-
-async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
-
-    const songInfo = await ytdl.getInfo(args[1]);
-    const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url
-    };
-
-    if (!serverQueue) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-        };
-
-        queue.set(message.guild.id, queueContruct);
-
-        queueContruct.songs.push(song);
-
-        try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            play(message.guild, queueContruct.songs[0]);
-        } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
-        }
-    } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(
-            `${song.title} has been added to the queue!`
-        );
-    }
-}
-
-function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could skip!");
-    serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
-    }
-
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(
-        `Start playing :arrow_forward: : **${song.title}**`
-    );
-}
-*/
 // Help Command
 client.on("message", async message => {
     if (!message.guild) return;
@@ -174,7 +45,7 @@ client.on("message", async message => {
 //Bot Join Command
 client.on("message", async message => {
     if (!message.guild) return;
-    if (message.content === "/waren") {
+    if (message.content === "/join") {
         if (message.member.voice.channel) {
             connection = await message.member.voice.channel.join();
         } else {
@@ -185,7 +56,7 @@ client.on("message", async message => {
 
 //Bot Leave Command
 client.on("message", async message => {
-    if (message.content === "/palayan") {
+    if (message.content === "/leave") {
         if (message.member.voice.channel) {
             connection = message.member.voice.channel.leave();
         }
@@ -313,7 +184,7 @@ client.on("message", async message => {
 
 client.on("message", async message => {
     if (!message.guild) return;
-    if (message.content === `${STREAM} kissfm`) {
+    if (message.content === `${STREAM} goldfm`) {
         if (message.member.voice.channel) {
             connection = await message.member.voice.channel.join();
             dispatcher = connection.play(goldRadio);
