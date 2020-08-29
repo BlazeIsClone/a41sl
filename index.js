33bceee343a85a973e05c2cca32bd2a3c6857725
Blaze <sandevabeykoon123@gmail.com>
2020-08-29 12:33:18 +0530

    ping command fixed STABLE

--- index.js
+++ index.js
@@ -36,11 +36,16 @@ client.once("disconnect", () => {
     console.log("Disconnect!");
 });
 
+//Ping Command
 client.on("message", async message => {
     if (!message.guild) return;
     if (message.content === "/ping") {
-        let command = require("./commands/ping.js");
-        command.run(client, message, args);
+        const msg = await message.channel.send("Ping?");
+        msg.edit(
+            `Latency is ${
+                msg.createdTimestamp - message.createdTimestamp
+            }ms. API Latency is ${Math.round(client.ping)}ms`
+        );
     }
 });
 
