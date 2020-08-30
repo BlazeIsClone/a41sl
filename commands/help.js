0af5948bee8b9f27b7d13b78c5a31352af58131a
Blaze <sandevabeykoon123@gmail.com>
2020-08-30 14:29:58 +0530

    fixed attachemnt test in help command

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
     aliases: ["h"],
     description: "Display all commands and descriptions",
     execute(message) {
-        let attachment = new MessageAttachment("../src/img/WELCOME_BANNER.png");
+        let attachment = new MessageAttachment("../src/img/SERVER_ICON.jpg");
         let helpEmbed = new MessageEmbed()
             .setTitle("👋  Hello There! All The Help You Need Is Here")
             .setThumbnail({
@@ -88,6 +88,6 @@ module.exports = {
                 "All For One Bot is still on alpha stage contribution would be appreciated ❤️"
             )
             .setTimestamp();
-        return message.author.send(attachment + helpEmbed).catch(console.error);
+        return message.author.send(attachment, helpEmbed).catch(console.error);
     }
 };
