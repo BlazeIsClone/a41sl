c33c6d11915eef2ff44aa62b2fd591010f742eee
Blaze <sandevabeykoon123@gmail.com>
2020-08-30 18:30:51 +0530

    fixed help command constructor issue

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const MessageEmbed = require("discord.js");
 
 const helpEmbed = new MessageEmbed()
     .setTitle("👋  Hello There! All The Help You Need Is Here")
