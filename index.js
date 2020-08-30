70040c5f4073c590cd9a35cb9b90b1733bcb4157
Blaze <sandevabeykoon123@gmail.com>
2020-08-30 18:45:14 +0530

    fixed messageEmbed issue with help command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { MessageEmbed, MessageAttachment, Collection } = require("discord.js");
+const { MessageAttachment, Collection } = require("discord.js");
+const MessageEmbed = require("discord.js");
 const Canvas = require("canvas");
 const token = process.env.DISCORD_TOKEN;
 const ytdl = require("ytdl-core");
@@ -56,7 +57,7 @@ client.on("message", async message => {
             "https://i.ibb.co/4RTP2QW/Discord-rulesguidelines.jpg"
         );
         //message.author.send(attachment);
-        message.author.send(helpEmbed).catch(console.error);
+        message.author.send(helpEmbed);
     }
 });
 
