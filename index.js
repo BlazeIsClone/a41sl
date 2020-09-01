169811663738af250d56075da949d41cd358e758
Blaze <sandevabeykoon123@gmail.com>
2020-09-01 08:23:42 +0530

    help command fixed and changed

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const helpAttachment = new MessageAttachment("https://i.imgur.com/790FtQS.png");
 client.on("message", async message => {
     if (!message.guild) return;
     if (message.content === "/help") {
-        message.author.send(helpEmbed);
+        message.author.send(helpEmbed).catch(console.error);
     }
 });
 
