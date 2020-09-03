35dfa2dcbe919577a59f10b8fd33999fe6ff32a6
Blaze <sandevabeykoon123@gmail.com>
2020-09-03 19:03:45 +0530

    fixed and modified reaction roel TEST!

--- index.js
+++ index.js
@@ -397,13 +397,16 @@ function generateMessages() {
         messages.push(`React below to get the **"${role}"** role!`);
     return messages;
 }
-client.on("message", member => {
-    let guild = client.guilds.cache.get("463027132243771403");
-    const roleChannel = member.guild.channels.cache.find(
-        ch => ch.name === "request-roles"
-    );
-});
+function triggerMsg() {
+    client.on("message", member => {
+        let guild = client.guilds.cache.get("463027132243771403");
+        const roleChannel = member.guild.channels.cache.find(
+            ch => ch.name === "request-roles"
+        );
+    });
+}
 client.on("message", message => {
+    triggerMsg();
     if (!roleChannel) return;
 
     var toSend = generateMessages();
