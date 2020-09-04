099a5e005342082d69bf270555faeeda28f8b4ca
Blaze <sandevabeykoon123@gmail.com>
2020-09-04 16:11:47 +0530

    modified load and track var in reaction-roles -f

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ var global = require("./global");
 require("dotenv").config();
 var bot = new Discord.Client();
 const config = require("./include/roles-reaction-db.json");
-var load = require("./src/load.js");
+var load = require("./src/load");
 load(bot, config);
-var track = require("./src/track.js");
+var track = require("./src/track");
 track(bot, config);
 
 client.once("ready", async () => {
