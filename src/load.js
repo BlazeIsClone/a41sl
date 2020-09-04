60bf56d074d99f5898b14dfcc50994d971d94875
Blaze <sandevabeykoon123@gmail.com>
2020-09-04 21:13:06 +0530

    await removed from laod js async function

--- src/load.js
+++ src/load.js
@@ -4,10 +4,10 @@
  * @returns {*} A proper emojiDiscriminator or null.
  */
 function cleanEmojiDiscriminator(emojiDiscriminator) {
-	var regEx = /[A-Za-z0-9_]+:[0-9]+/;
-	var cleaned = regEx.exec(emojiDiscriminator);
-	if (cleaned) return cleaned[0];
-	return emojiDiscriminator;
+    var regEx = /[A-Za-z0-9_]+:[0-9]+/;
+    var cleaned = regEx.exec(emojiDiscriminator);
+    if (cleaned) return cleaned[0];
+    return emojiDiscriminator;
 }
 
 /**
@@ -15,39 +15,45 @@ function cleanEmojiDiscriminator(emojiDiscriminator) {
  * @param {*} client The bot client.
  * @param {*} config The config file.
  */
-module.exports = function(client, config) {
-	
-	client
-		.on("ready", async () => {
-			//Bot ready
-			console.log("Fetching messages");
-			console.log("Note: If the next message does not say \"ASYNC IIFE working!\", you have to update to Node 7.6.0 or later.");
-			(async () => {
-				var debug_count_messagesFetched = 0;
-				console.log("ASYNC IIFE working!");
-				for (var { channel, message: message_id, reactions } of config) {
-					var message = await client.channels.get(channel).fetchMessage(message_id)
-						.catch(error => console.error(error));
-					if (!message) continue;
-					debug_count_messagesFetched += 1;
-					for (var {emoji} of reactions) {
-						emoji = cleanEmojiDiscriminator(emoji);
-						var messageReaction = message.reactions.get(emoji);
-						if (!messageReaction) {
-							await message.react(emoji)
-								.catch(error => console.error(error));
-							//No fetch necessary since no prior existing reactions.
-						} else {
-							if (!messageReaction.me) {
-								//Fetch each reaction into cache to keep track of them
-								messageReaction.fetchUsers();
-								await message.react(emoji)
-									.catch(error => console.error(error));
-							}
-						}
-					}
-				}
-				console.log(`Done fetching ${debug_count_messagesFetched} message(s).`);
-			})();
-		});
+module.exports = function (client, config) {
+    client.on("ready", () => {
+        //Bot ready
+        console.log("Fetching messages");
+        console.log(
+            'Note: If the next message does not say "ASYNC IIFE working!", you have to update to Node 7.6.0 or later.'
+        );
+        (async () => {
+            var debug_count_messagesFetched = 0;
+            console.log("ASYNC IIFE working!");
+            for (var { channel, message: message_id, reactions } of config) {
+                var message = client.channels
+                    .get(channel)
+                    .fetchMessage(message_id)
+                    .catch(error => console.error(error));
+                if (!message) continue;
+                debug_count_messagesFetched += 1;
+                for (var { emoji } of reactions) {
+                    emoji = cleanEmojiDiscriminator(emoji);
+                    var messageReaction = message.reactions.get(emoji);
+                    if (!messageReaction) {
+                        await message
+                            .react(emoji)
+                            .catch(error => console.error(error));
+                        //No fetch necessary since no prior existing reactions.
+                    } else {
+                        if (!messageReaction.me) {
+                            //Fetch each reaction into cache to keep track of them
+                            messageReaction.fetchUsers();
+                            await message
+                                .react(emoji)
+                                .catch(error => console.error(error));
+                        }
+                    }
+                }
+            }
+            console.log(
+                `Done fetching ${debug_count_messagesFetched} message(s).`
+            );
+        })();
+    });
 };
