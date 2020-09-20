a82cd4fbc0d2831d44cea39af3883de98180197b
Blaze <sandevabeykoon123@gmail.com>
2020-09-20 15:21:48 +0530

    updated init shell script with webhook direct msg functionality

--- init.sh
+++ init.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-echo Hello World
+#!/bin/bash
+
+DISCORD_USERNAME=${DISCORD_USERNAME:-"All For One Bot CLI Notifier "}
+DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL:-"https://discordapp.com/api/webhooks/757175037085220975/BFgdAwXrBzvhWbQXrRy4DS6iA7bKk_keq4JzB9GVDJR44MA60SF7QdPtH62sdjR7jxl_"}
+
+if [ -z "$DISCORD_WEBHOOK_URL" ]; then
+     echo "You must define DISCORD_WEBHOOK_URL in order to post a message";
+      exit 1;
+      fi
+     
+     curl -X POST --data-urlencode 'payload_json={"username": "'"$DISCORD_USERNAME"'", "content":"'"Hello, Discord"'"}' $DISCORD_WEBHOOK_URL 
