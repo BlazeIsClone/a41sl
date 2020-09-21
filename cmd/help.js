a19f952d4ac60a67821710d20ceadc1ab23c2a54
Blaze <sandevabeykoon123@gmail.com>
2020-09-21 23:19:39 +0530

    updated cmd/help error msg

--- cmd/help.js
+++ cmd/help.js
@@ -1,11 +1,16 @@
 const Discord = require("discord.js");
 module.exports.run = (client, message, args) => {
+    if (!message.channel.nsfw)
+        return message.channel.send(
+            ":underage:  The Help Command For NSFW Content Is Only Allowed In NSFW Channels!"
+        );
+
     var help = new Discord.MessageEmbed()
         .setAuthor("Help :")
         .setDescription("**Prefix : /nsfw **\n List of Commands!")
         .addField(
             "NSFW  :underage: :",
             "`4k`, `anal`, `ass`, `hentai`, `hkitsune`, `hneko`, `holo`, `kemonomimi`, `neko`, `pussy`"
-        )
+        );
     message.channel.send(help);
 };
