5f2d9b2479340832f0e82a0d5a79907f8eace825
Blaze <sandevabeykoon123@gmail.com>
2020-09-21 23:27:02 +0530

    updated NSFW HELP string

--- cmd/help.js
+++ cmd/help.js
@@ -6,10 +6,10 @@ module.exports.run = (client, message, args) => {
         );
 
     var help = new Discord.MessageEmbed()
-        .setAuthor(":underage:  NSFW Help :")
+        .setAuthor("NSFW Help :")
         .setDescription("**Prefix : /nsfw **\n List of Commands!")
         .addField(
-            "NSFW :",
+            ":underage:  NSFW :",
             "`4k`, `anal`, `ass`, `hentai`, `hkitsune`, `hneko`, `holo`, `kemonomimi`, `neko`, `pussy`"
         );
     message.channel.send(help);
