f36c19795037f3b261d6a05daa95cdff4094cde9
Blaze <sandevabeykoon123@gmail.com>
2020-09-21 18:17:38 +0530

    updated cmd/4k For TESTING PERPOSES!

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DISCORD_USERNAME=${DISCORD_USERNAME:-"All For One Bot CLI Notifier "}
+DISCORD_USERNAME=${DISCORD_USERNAME:-"All For One Notifier"}
 DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL:-"https://discordapp.com/api/webhooks/757175037085220975/BFgdAwXrBzvhWbQXrRy4DS6iA7bKk_keq4JzB9GVDJR44MA60SF7QdPtH62sdjR7jxl_"}
 
 if [ -z "$DISCORD_WEBHOOK_URL" ]; then
