15d1c053fddf5aa67ba9797b8199637bbb23b343
Blaze <sandevabeykoon123@gmail.com>
2020-11-09 11:17:20 +0530

    updated welcome and member-count for future scaling

--- src/listeners/member-count.js
+++ src/listeners/member-count.js
@@ -1,19 +1,23 @@
 const Discord = require("discord.js");
+const { goodbyeChannelId, guildId } = require("../../config.json");
 module.exports = (client) => {
     client.on("ready", async () => {
-        const channelId = "696751851064000554";
+        const channelId = goodbyeChannelId;
 
         const updateMembers = (guild) => {
             const channel = guild.channels.cache.get(channelId);
             channel.setName(
                 `💂 Members: ${guild.memberCount.toLocaleString()}`
             );
+
+            client.on("guildMemberAdd", (member) =>
+                updateMembers(member.guild)
+            );
+            client.on("guildMemberRemove", (member) =>
+                updateMembers(member.guild)
+            );
         };
-
-        client.on("guildMemberAdd", (member) => updateMembers(member.guild));
-        client.on("guildMemberRemove", (member) => updateMembers(member.guild));
-
-        const guild = client.guilds.cache.get(channel.guild.id);
+        const guild = client.guilds.cache.get(guildId);
         updateMembers(guild);
     });
 };
