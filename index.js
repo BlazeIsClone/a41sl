a894a2b01dc41d7730b7c9d0574e5ec2388c35be
Blaze <sandevabeykoon123@gmail.com>
2020-11-12 13:31:40 +0530

    updated dependencies added fitgirl webhook for testing

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const reactionRoles = require("./src/commands/admin/reactionRoles");
 const musicCommands = require("./src/commands/musicCommands");
 const dankmemes = require("./src/commands/memes/dankmemes");
 const announcementsWebhook = require("./src/webhooks/Announcements");
+const fitgirl = require("./src/webhooks/fitgirl");
 
 load(client, config);
 track(client, config);
@@ -53,6 +54,7 @@ reactionRoles(client);
 musicCommands(client);
 dankmemes(client);
 announcementsWebhook(client);
+//fitgirl(client);
 
 client.commands = new Collection();
 client.prefix = PREFIX;
