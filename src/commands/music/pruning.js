78bfd549f9c0b950157b6bfce1b3b26f48327bd5
Blaze <sandevabeykoon123@gmail.com>
2020-11-17 15:23:58 +0530

    formated pruning command

--- src/commands/music/pruning.js
+++ src/commands/music/pruning.js
@@ -7,21 +7,25 @@ module.exports = {
     execute(message) {
         config.PRUNING = !config.PRUNING;
 
-        fs.writeFile("./config.json", JSON.stringify(config, null, 2), err => {
-            if (err) {
-                console.log(err);
+        fs.writeFile(
+            "./config.json",
+            JSON.stringify(config, null, 2),
+            (err) => {
+                if (err) {
+                    console.log(err);
+                    return message.channel
+                        .send("There was an error writing to the file.")
+                        .catch(console.error);
+                }
+
                 return message.channel
-                    .send("There was an error writing to the file.")
+                    .send(
+                        `Message pruning is ${
+                            config.PRUNING ? "**enabled**" : "**disabled**"
+                        }`
+                    )
                     .catch(console.error);
             }
-
-            return message.channel
-                .send(
-                    `Message pruning is ${
-                        config.PRUNING ? "**enabled**" : "**disabled**"
-                    }`
-                )
-                .catch(console.error);
-        });
-    }
+        );
+    },
 };
