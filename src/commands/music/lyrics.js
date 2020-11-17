550fbb113014035484a93ee857e5cb969fa930c5
Blaze <sandevabeykoon123@gmail.com>
2020-11-17 14:16:58 +0530

    updated lyrics command

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -2,37 +2,30 @@ const { MessageEmbed } = require("discord.js");
 const lyricsFinder = require("lyrics-finder");
 
 module.exports = {
-    name: "lyrics",
-    aliases: ["ly"],
-    description: "Get lyrics for the currently playing song",
-    async execute(message) {
-        const queue = message.client.queue.get(message.guild.id);
-        if (!queue)
-            return message.channel
-                .send("There is nothing playing.")
-                .catch(console.error);
+  name: "lyrics",
+  aliases: ["ly"],
+  description: "Get lyrics for the currently playing song",
+  async execute(message) {
+      const queue = message.client.queue.get(message.guild.id);
+      if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
 
-        let lyrics = null;
+      let lyrics = null;
 
-        try {
+      try {
             lyrics = await lyricsFinder(queue.songs[0].title, "");
-            if (!lyrics)
+            if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+          } catch (error) {
                 lyrics = `No lyrics found for ${queue.songs[0].title}.`;
-        } catch (error) {
-            lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+              }
+
+      let lyricsEmbed = new MessageEmbed()
+        .setTitle(`${queue.songs[0].title} — Lyrics`)
+        .setDescription(lyrics)
+        .setColor("#F8AA2A")
+        .setTimestamp();
+
+      if (lyricsEmbed.description.length >= 2048)
+        lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+          return message.channel.send(lyricsEmbed).catch(console.error);
         }
-
-        let lyricsEmbed = new MessageEmbed()
-            .setTitle("Lyrics")
-            .setDescription(lyrics)
-            .setColor("#F8AA2A")
-            .setTimestamp();
-
-        if (lyricsEmbed.description.length >= 2048)
-            lyricsEmbed.description = `${lyricsEmbed.description.substr(
-                0,
-                2045
-            )}...`;
-        return message.channel.send(lyricsEmbed).catch(console.error);
-    }
 };
