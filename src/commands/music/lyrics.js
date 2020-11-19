560b7f58ecf18af253522a3266217836c2b8983c
Blaze <sandevabeykoon123@gmail.com>
2020-11-19 16:18:05 +0530

    update lyrics & nowplaying embed

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -6,26 +6,31 @@ module.exports = {
   aliases: ["ly"],
   description: "Get lyrics for the currently playing song",
   async execute(message) {
-      const queue = message.client.queue.get(message.guild.id);
-      if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
+    const queue = message.client.queue.get(message.guild.id);
+    const emptyQueue = new MessageEmbed()
+      .setColor(0xda7272)
+      .setTimestamp()
+      .setTitle("Empty Queue")
+      .setDescription("There is nothing playing");
 
-      let lyrics = null;
+    if (!queue) return message.channel.send(emptyQueue);
+    let lyrics = null;
 
-      try {
-            lyrics = await lyricsFinder(queue.songs[0].title, "");
-            if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
-          } catch (error) {
-                lyrics = `No lyrics found for ${queue.songs[0].title}.`;
-              }
+    try {
+      lyrics = await lyricsFinder(queue.songs[0].title, "");
+      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    } catch (error) {
+      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
 
-      let lyricsEmbed = new MessageEmbed()
-        .setTitle(`${queue.songs[0].title} — Lyrics`)
-        .setDescription(lyrics)
-        .setColor("#F8AA2A")
-        .setTimestamp();
+    let lyricsEmbed = new MessageEmbed()
+      .setTitle(`${queue.songs[0].title} — Lyrics`)
+      .setDescription(lyrics)
+      .setColor("#F8AA2A")
+      .setTimestamp();
 
-      if (lyricsEmbed.description.length >= 2048)
-        lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
-          return message.channel.send(lyricsEmbed).catch(console.error);
-        }
+    if (lyricsEmbed.description.length >= 2048)
+      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+    return message.channel.send(lyricsEmbed).catch(console.error);
+  },
 };
