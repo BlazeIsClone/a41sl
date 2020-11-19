c8020e85d648123d577007f382cf54190934bd8f
Blaze <sandevabeykoon123@gmail.com>
2020-11-19 16:46:27 +0530

    add embeds to uptime command

--- src/commands/music/uptime.js
+++ src/commands/music/uptime.js
@@ -1,20 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
   name: "uptime",
 
   aliases: ["u"],
   description: "Check the uptime",
   execute(message) {
-      let seconds = Math.floor(message.client.uptime / 1000);
-      let minutes = Math.floor(seconds / 60);
-      let hours = Math.floor(minutes / 60);
-      let days = Math.floor(hours / 24);
+    let seconds = Math.floor(message.client.uptime / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
 
-      seconds %= 60;
-      minutes %= 60;
-      hours %= 24;
+    seconds %= 60;
+    minutes %= 60;
+    hours %= 24;
 
-      return message
-        .reply(`⏱ Uptime: \`${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\``)
-            .catch(console.error);
-        }
+    const uptimeR = new MessageEmbed()
+      .setColor(0x7289da)
+      .setTimestamp()
+      .setTitle("Uptime Duration")
+      .setDescription(
+        `**${days}** day(s), **${hours}** hours, **${minutes}** minutes, **${seconds}** seconds`
+      );
+
+    return message.reply(uptimeR).catch(console.error);
+  },
 };
