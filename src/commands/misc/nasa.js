1e12e3b4fa117752b4afeca131e2404228c16ce3
Blaze <sandevabeykoon123@gmail.com>
2020-11-22 14:12:44 +0530

    add: nasa cmd help embed

--- src/commands/misc/nasa.js
+++ src/commands/misc/nasa.js
@@ -7,9 +7,16 @@ const { PREFIX } = require("../../../config.json");
 
 module.exports = async (client) => {
   client.on("message", async (message) => {
+    const useCmd = new MessageEmbed()
+      .setDescription("Usage: /nasa <question> ")
+      .setTitle("NASA");
+    if (message.content === PREFIX + "nasa") {
+      message.reply(useCmd).catch(console.error);
+    }
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
     let args = message.content.slice(PREFIX.length).trim().split(/ +/);
     let command = args.shift().toLowerCase();
+
     if (command === "nasa") {
       if (!args) {
         message.channel.send("Enter a valid term to search for!");
