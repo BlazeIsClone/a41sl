482b826cd1d301ae5ba3104efb061eee47307428
Blaze <sandevabeykoon123@gmail.com>
2020-11-22 17:34:48 +0530

    update: nasa cmd err handler

--- src/commands/misc/nasa.js
+++ src/commands/misc/nasa.js
@@ -7,19 +7,20 @@ const { PREFIX } = require("../../../config.json");
 
 module.exports = async (client) => {
   client.on("message", async (message) => {
-    const useCmd = new MessageEmbed()
-      .setDescription("Usage: /nasa <question> ")
-      .setTitle("NASA");
-    if (message.content === PREFIX + "nasa") {
-      message.reply(useCmd).catch(console.error);
-    }
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
     let args = message.content.slice(PREFIX.length).trim().split(/ +/);
     let command = args.shift().toLowerCase();
+    if (message.content === PREFIX + "nasa") {
+      const useCmd = new MessageEmbed()
+        .setDescription("Usage: /nasa <question> ")
+        .setTitle("NASA");
+      message.reply(useCmd).catch(console.error);
+    }
 
     if (command === "nasa") {
       if (!args) {
         message.channel.send("Enter a valid term to search for!");
+        return;
       }
       let term = args.join(" ");
       let response = await fetch(
@@ -27,10 +28,10 @@ module.exports = async (client) => {
       );
       let data = await response.json();
       if (!data.collection.items[0].data[0].description) {
-        let msg = await message.channel.send(
-          `Couldn't find any results for ${term}`
-        );
-        msg.delete({ timeout: 10000 });
+        let msg = await message.channel
+          .send(`Couldn't find any results for ${term}`)
+          .catch(console.error);
+        msg.delete({ timeout: 10000 }).catch(console.error);
         return message.react("❌");
       }
       let nasasearchembed = new Discord.MessageEmbed()
@@ -39,8 +40,12 @@ module.exports = async (client) => {
         .setDescription(data.collection.items[0].data[0].description)
         .setImage(data.collection.items[0].links[0].href.split(" ").join("%20"))
         .setTimestamp();
-      await message.channel.send(nasasearchembed).catch(console.error);
-      message.react("✔️");
+      try {
+        await message.channel.send(nasasearchembed).catch(console.error);
+        message.react("✔️");
+      } catch (e) {
+        console.log(e);
+      }
     }
   });
 };
