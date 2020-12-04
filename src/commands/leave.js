78369fd35048885c27c2a1f1a0f96f83d7085648
Blaze <sandevabeykoon123@gmail.com>
2020-12-04 10:35:08 +0530

    update: leave cmd perms & cmd handler

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,14 +1,21 @@
 const Discord = require("discord.js");
+const { PREFIX } = require("../../config.json");
 
 module.exports = (client) => {
     client.on("message", async (message) => {
-        if (message.content === "/leave") {
+        if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+        let args = message.content.slice(PREFIX.length).trim().split(/ +/);
+        let command = args.shift().toLowerCase();
+        if (command === "leave") {
+            if (!message.member.hasPermission("CONNECT"))
+                return message.reply(
+                    "You do not have the permissions to do that"
+                );
+
             if (message.member.voice.channel) {
                 connection = message.member.voice.channel.leave();
             } else {
-                message.reply(
-                    "You have to be in a voice channel to play music"
-                );
+                message.reply("Im not in this voice channel to leave!");
             }
         }
     });
