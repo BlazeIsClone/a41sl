7caa329fc2cdfd370e77f18120e107facef08210
Blaze <sandevabeykoon123@gmail.com>
2020-12-04 10:30:54 +0530

    update: join cmd permissions & cmd handler

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,8 +1,17 @@
 const Discord = require("discord.js");
+const { PREFIX } = require("../../config.json");
 
 module.exports = (client) => {
     client.on("message", async (message) => {
-        if (message.content === "/join") {
+        if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+        let args = message.content.slice(PREFIX.length).trim().split(/ +/);
+        let command = args.shift().toLowerCase();
+        if (command === "join") {
+            if (!message.member.hasPermission("CONNECT"))
+                return message.reply(
+                    "You do not have the permissions to do that"
+                );
+
             if (message.member.voice.channel) {
                 connection = await message.member.voice.channel.join();
             } else {
