14918146b6511a0010a773bd3587cb134aaeb29e
Blaze <sandevabeykoon123@gmail.com>
2020-12-14 01:14:20 +0530

    update: nowplaying cmd

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -29,24 +29,25 @@ module.exports = {
         const left = song.duration - seek;
 
         let nowPlaying = new MessageEmbed()
-            .setTitle("Now playing")
-            .setDescription(`[${song.title}](${song.url})`)
-            .setColor("#F8AA2A")
+            .setDescription(`**[${song.title}](${song.url})**`)
+            .setColor("#00FF00")
             .addField(
                 "\u200b",
-                new Date(seek * 1000).toISOString().substr(11, 8) +
+                createBar(
+                    song.duration == 0 ? seek : song.duration,
+                    seek,
+                    20
+                )[0] +
+                    "\n" +
                     "[" +
-                    createBar(
-                        song.duration == 0 ? seek : song.duration,
-                        seek,
-                        20
-                    )[0] +
-                    "]" +
+                    new Date(seek * 1000).toISOString().substr(11, 8) +
+                    "/" +
                     (song.duration == 0
                         ? " ◉ LIVE"
                         : new Date(song.duration * 1000)
                               .toISOString()
-                              .substr(11, 8)),
+                              .substr(11, 8)) +
+                    "]",
                 false
             );
 
