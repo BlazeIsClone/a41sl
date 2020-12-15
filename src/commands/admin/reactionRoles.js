f9cb3eb2340b713789d37fb7a7a8a62a1c4a9bc3
Blaze <sandevabeykoon123@gmail.com>
2020-12-15 23:39:05 +0530

    update: reactionRoles Embed with emojies and roels

--- src/commands/admin/reactionRoles.js
+++ src/commands/admin/reactionRoles.js
@@ -1,59 +1,24 @@
 const Discord = require("discord.js");
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 module.exports = (client) => {
-    const addRolesAttachment = new MessageAttachment(
-        "https://i.imgur.com/XzvxWtQ.png"
-    );
+    const addRolesAttachment = new MessageEmbed()
+        .setImage("https://i.imgur.com/XzvxWtQ.png")
+        .setColor("#00FF00");
     client.on("message", async (message) => {
         if (!message.guild) return;
         if (message.content === "//rolesEmbed") {
             const addRolesEmbed = new Discord.MessageEmbed()
 
-                .setColor("#0099ff")
-                .setDescription(
+                .setColor("#00FF00")
+                .setTitle(
                     "**To join or leave a role, react to this message with one of the following emotes.**"
                 )
-                .addFields(
-                    {
-                        name: "​",
-                        value: "​",
-                    },
-                    {
-                        name: ":lol: League of Legends",
-                        value: "​",
-                    },
-                    {
-                        name: ":csgo~1: CSGO",
-                        value: "​",
-                    },
-                    {
-                        name: ":r6: Rainbow Six Siege",
-                        value: "​",
-                    },
-                    {
-                        name: ":amongus: Among Us",
-                        value: "​",
-                    },
-                    {
-                        name: ":gta5: GTA Online",
-                        value: "​",
-                    },
-                    {
-                        name: ":wow: World of Warcraft",
-                        value: "​",
-                    },
-                    {
-                        name: ":fortnite: Fortnite",
-                        value: "​",
-                    },
-                    {
-                        name: ":brawlhalla: Brawlhalla",
-                        value: "​",
-                    }
+                .setDescription(
+                    "\n<:lol:788452092935012362> • League of Legends\n\n<:csgo:788451154576408577> • CSGO\n\n<:r6:788451153523769355> • rainbow Six Siege\n\n<:amongus:788451152768663642> • Among Us\n\n<:gta5:788451153394270208> • GTA Online\n\n<:wow:788451154362368020> • World of Warcraft\n\n<:fortnite:788451152752672778> • Fortnite\n\n<:brawlhalla:788451163300167770> • Brawlhalla"
                 );
 
             (async () => {
-                await message.author
+                await message.channel
                     .send(addRolesAttachment)
                     .catch(console.error);
                 try {
