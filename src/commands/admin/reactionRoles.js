2a9acf8def87e5ad7c12feef27813bfff12d20dd
Blaze <sandevabeykoon123@gmail.com>
2020-12-16 09:50:26 +0530

    update: reaction Roles Admin cmd updated add new game roles

--- src/commands/admin/reactionRoles.js
+++ src/commands/admin/reactionRoles.js
@@ -1,20 +1,50 @@
 const Discord = require("discord.js");
 const { MessageEmbed, MessageAttachment } = require("discord.js");
+const { PREFIX } = require("../../../config.json");
 module.exports = (client) => {
-    const addRolesAttachment = new MessageEmbed()
-        .setImage("https://i.imgur.com/XzvxWtQ.png")
-        .setColor("#00FF00");
     client.on("message", async (message) => {
-        if (!message.guild) return;
-        if (message.content === "//rolesEmbed") {
-            const addRolesEmbed = new Discord.MessageEmbed()
+        const addRolesAttachment = new MessageEmbed()
+            .setImage("https://i.imgur.com/XzvxWtQ.png")
+            .setColor("#00FF00");
 
+        if (!message.guild) return;
+        if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+        let args = message.content.slice(PREFIX.length).trim().split(/ +/);
+        let command = args.shift().toLowerCase();
+        if (command === "rolesembed") {
+            if (!message.member.hasPermission("ADMINISTRATOR"))
+                return message.reply(
+                    "You do not have the permissions to do that"
+                );
+
+            const addRolesEmbed = new Discord.MessageEmbed()
                 .setColor("#00FF00")
                 .setTitle(
-                    "**To join or leave a role, react to this message with one of the following emotes.**"
+                    "**React to this message with one of the following games you play.**"
                 )
                 .setDescription(
-                    "\n<:lol:788452092935012362> • League of Legends\n\n<:csgo:788451154576408577> • CSGO\n\n<:r6:788451153523769355> • rainbow Six Siege\n\n<:amongus:788451152768663642> • Among Us\n\n<:gta5:788451153394270208> • GTA Online\n\n<:wow:788451154362368020> • World of Warcraft\n\n<:fortnite:788451152752672778> • Fortnite\n\n<:brawlhalla:788451163300167770> • Brawlhalla"
+                    "\n" +
+                        "<:lol:788452092935012362> • League of Legends" +
+                        "\n\n" +
+                        "<:csgo:788451154576408577> • CSGO" +
+                        "\n\n" +
+                        "<:r6:788451153523769355> • rainbow Six Siege" +
+                        "\n\n" +
+                        "<:amongus:788451152768663642> • Among Us" +
+                        "\n\n" +
+                        "<:gta5:788451153394270208> • GTA Online" +
+                        "\n\n" +
+                        "<:wow:788451154362368020> • World of Warcraft" +
+                        "\n\n" +
+                        "<:fortnite:788451152752672778> • Fortnite" +
+                        "\n\n" +
+                        "<:brawlhalla:788451163300167770> • Brawlhalla" +
+                        "\n\n" +
+                        "<:rust:788452945104470017> • Rust" +
+                        "\n\n" +
+                        "<:minecraft:788618024260861972> • Minecraft" +
+                        "\n\n" +
+                        "<:dota2:788615300370530305> • Dota 2"
                 );
 
             (async () => {
