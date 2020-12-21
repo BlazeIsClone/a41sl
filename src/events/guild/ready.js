27178bbf5ae3562d24a93d3489174a5042e52a61
Blaze <sandevabeykoon123@gmail.com>
2020-12-21 18:40:14 +0530

    fix: memes EVENT

--- src/events/guild/ready.js
+++ src/events/guild/ready.js
@@ -1,6 +1,37 @@
 const Discord = require("discord.js");
-const { memberCountChannelId, guildId } = require("../../../config.json");
+const request = require("request");
+const { MessageEmbed } = require("discord.js");
+const {
+    memesChannel,
+    memberCountChannelId,
+    guildId,
+} = require("../../../config.json");
+
 module.exports = async (client) => {
+    console.log("MemesAPI ready");
+    var interval = setInterval(function () {
+        let urls = [
+            "https://meme-api.herokuapp.com/gimme/dankmemes",
+            "https://meme-api.herokuapp.com/gimme/wholesomememes",
+            "https://meme-api.herokuapp.com/gimme/memes",
+        ];
+
+        let subreddit = urls[Math.floor(Math.random() * urls.length)];
+        return request(subreddit, (err, response, body) => {
+            if (err) throw err;
+            var data = JSON.parse(body);
+
+            let meme = new Discord.MessageEmbed()
+                .setDescription(`**[${data.title}](${data.postLink})**`)
+                .setImage(data.url)
+                .setFooter(`👍 ${data.ups}`)
+                .setColor("#32CD32");
+
+            const memeSendChannel = client.channels.cache.get(memesChannel);
+            memeSendChannel.send(meme).catch(console.error);
+        });
+    }, 1 * 3600000);
+
     const channelId = memberCountChannelId;
 
     const updateMembers = (guild) => {
