e6500b022d88e5853ec561d8f197f61f069b25b4
Blaze <sandevabeykoon123@gmail.com>
2020-12-24 20:04:37 +0530

    update: lyrics cmd

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -2,35 +2,40 @@ const { MessageEmbed } = require("discord.js");
 const lyricsFinder = require("lyrics-finder");
 const musicChannel = process.env.MUSIC_CHANNEL;
 
-module.exports.run = async (client, message) => {
-  if (message.channel.id != musicChannel) {
-    return message.author.send(
-      "⛔ Music commands are only available in **add-music** channel"
-    );
-  }
+module.exports = {
+  name: "lyrics",
+  aliases: ["ly"],
+  description: "Get lyrics for the currently playing song",
+  async execute(message) {
+    if (message.channel.id != musicChannel) {
+      return message.author.send(
+        "⛔ Music commands are only available in **add-music** channel"
+      );
+    }
 
-  const queue = message.client.queue.get(message.guild.id);
-  const emptyQueue = new MessageEmbed()
-    .setColor(0xda7272)
-    .setTitle("Empty Queue")
-    .setDescription("There is nothing playing");
+    const queue = message.client.queue.get(message.guild.id);
+    const emptyQueue = new MessageEmbed()
+      .setColor(0xda7272)
+      .setTitle("Empty Queue")
+      .setDescription("There is nothing playing");
 
-  if (!queue) return message.channel.send(emptyQueue);
-  let lyrics = null;
+    if (!queue) return message.channel.send(emptyQueue);
+    let lyrics = null;
 
-  try {
-    lyrics = await lyricsFinder(queue.songs[0].title, "");
-    if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
-  } catch (error) {
-    lyrics = `No lyrics found for ${queue.songs[0].title}.`;
-  }
+    try {
+      lyrics = await lyricsFinder(queue.songs[0].title, "");
+      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    } catch (error) {
+      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
 
-  let lyricsEmbed = new MessageEmbed()
-    .setTitle(`${queue.songs[0].title} — Lyrics`)
-    .setDescription(lyrics)
-    .setColor("#F8AA2A");
+    let lyricsEmbed = new MessageEmbed()
+      .setTitle(`${queue.songs[0].title} — Lyrics`)
+      .setDescription(lyrics)
+      .setColor("#F8AA2A");
 
-  if (lyricsEmbed.description.length >= 2048)
-    lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
-  return message.channel.send(lyricsEmbed).catch(console.error);
+    if (lyricsEmbed.description.length >= 2048)
+      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+    return message.channel.send(lyricsEmbed).catch(console.error);
+  },
 };
