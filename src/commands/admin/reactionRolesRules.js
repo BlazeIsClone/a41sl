dd1198693d8c3199d4cadc199de6a7a0e2983acb
Blaze <sandevabeykoon123@gmail.com>
2021-02-05 09:48:31 +0530

    update:(reactionRolesRules & database) update rules and removed cross reaction input

--- src/commands/admin/reactionRolesRules.js
+++ src/commands/admin/reactionRolesRules.js
@@ -2,97 +2,57 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: "reactionRolesRules",
+    name: "rrr",
     description: "Sends an embed with a rules",
     async execute(message, args) {
         const rulesEmbed = new Discord.MessageEmbed()
             .setColor("#00FF00")
-            .setTitle(
-                "Here you can meet new people and game with everyone and anyone you know. Use the server responsibly and have fun!"
-            )
             .setDescription(
-                "No blank nicknames." +
+                "Here you can meet new people and game with everyone and anyone you know. Use the server responsibly and have fun!" +
+                    "\n\n" +
+                    "**1. Be kind**" +
                     "\n" +
-                    "No inappropriate nicknames." +
+                    "Treat others with kindness and respect, please don’t be rude to others. We want this server to be a place where you can join and chat with people with similar interests. Nobody comes here to argue with others on purpose." +
+                    "\n\n" +
+                    "**2. Advertisemen**t" +
                     "\n" +
-                    "No sexually explicit nicknames." +
+                    "Only Promote social media content in the advertising category. DM advertising is not allowed according to Discord's Terms of Service." +
                     "\n" +
-                    "No offensive nicknames." +
+                    "**3. Do not spam**" +
+                    "\n\n" +
+                    "To keep our server satisfied, we would appreciate it if you do not spam. This includes pinging a member for no reason and posting text walls. We will not hesitate to mute or ban you." +
+                    "\n\n" +
+                    "**4. Content Posting**" +
                     "\n" +
-                    "No inappropriate profile pictures." +
+                    "Keep an eye on the content you post and it must be relevant to the text-channel in which your posting. You may not share content that glorifies or promotes suicide or self-harm." +
+                    "\n\n" +
+                    "**5. No Inappropriate User Profiles**" +
                     "\n" +
-                    "No sexually explicit profile pictures." +
+                    "For ease of communication and the comfort of those in chat, the profile picture, custom status, and display name should be in line with the Discord community guidelines. Furthermore, the display name should also be easily readable, mentionable and appropriate." +
+                    "\n\n" +
+                    "**6. No Scamming**" +
                     "\n" +
-                    "No offensive profile pictures." +
+                    "Any member caught scamming will be banned. It's the internet; not everyone will be friendly or have good intentions. Please, be cautious and use common sense." +
+                    "\n\n" +
+                    "**7. Follow the Discord ToS and Community Guidelines**" +
                     "\n" +
-                    "Moderators reserve the right to change nicknames." +
+                    "It’s stated on their website, follow them. They are made to be followed." +
                     "\n" +
-                    "Moderators reserve the right to use their own discretion regardless of any rule." +
+                    "Terms of Service: https://discordapp.com/terms" +
                     "\n" +
-                    "No exploiting loopholes in the rules (please report them)." +
-                    "\n" +
-                    "No inviting unofficial bots." +
-                    "\n" +
-                    "No bugs, exploits, glitches, hacks, bugs, etc." +
-                    "\n" +
-                    "No questioning the mods." +
-                    "\n" +
-                    "No @mentioning the mods." +
-                    "\n" +
-                    "No asking to be granted roles/moderator roles." +
-                    "\n" +
-                    "Contact the moderators under #request-support for support." +
-                    "\n" +
-                    "No @everyone/@here mentioning without permission." +
-                    "\n" +
-                    "No @mentioning spam." +
-                    "\n" +
-                    "No illegal content." +
-                    "\n" +
-                    "No hacking." +
-                    "\n" +
-                    "No publishing of personal information (including real names, addresses, emails, passwords, bank account and credit card information, etc.)." +
-                    "\n" +
-                    "No personal attacks." +
-                    "\n" +
-                    "No witch hunting." +
-                    "\n" +
-                    "No harassment." +
-                    "\n" +
-                    "No sexism." +
-                    "\n" +
-                    "No racism." +
-                    "\n" +
-                    "No hate speech." +
-                    "\n" +
-                    "No religious discussions." +
-                    "\n" +
-                    "No political discussions." +
-                    "\n" +
-                    "No spamming." +
-                    "\n" +
-                    "No overusing emojis." +
-                    "\n" +
-                    "No overusing reactions." +
-                    "\n" +
-                    "Moderators reserve the right to delete any post." +
-                    "\n" +
-                    "No advertisement without permission." +
-                    "\n" +
-                    "No linking to other servers." +
-                    "\n" +
-                    "Use the right text channel for the topic you wish to discuss." +
-                    "\n" +
-                    "No annoying, loud or high pitch noises." +
-                    "\n" +
-                    "Reduce the amount of background noise, if possible." +
-                    "\n" +
-                    "Moderators reserve the right to disconnect, mute, deafen, or move members to and from voice channels."
+                    "Guidelines: https://discordapp.com/guidelines"
+            );
+
+        const rulesEmbed2 = new Discord.MessageEmbed()
+            .setColor("#00FF00")
+            .setDescription(
+                "If you agree to all the rules above, click on the ✅ at the end of this post to gain access to the rest of the server. After doing so, you can head to <#751076769486078062> to assign yourself personalized roles!"
             );
         const rulesAttachment = new MessageEmbed()
             .setImage("https://i.imgur.com/TBGYIJ7.png")
             .setColor("#00FF00");
         message.channel.send(rulesAttachment);
         message.channel.send(rulesEmbed);
+        message.channel.send(rulesEmbed2);
     },
 };
