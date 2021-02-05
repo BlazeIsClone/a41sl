00f75ab8d8bf09783e2d13552c3b7b9c79b00665
Blaze <sandevabeykoon123@gmail.com>
2021-02-05 20:21:57 +0530

    update:(.env.example/config.json.example/notificationWebhook.sh) modified env vars

--- src/webhooks/notificationWebhook.sh
+++ src/webhooks/notificationWebhook.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DISCORD_USERNAME=${DISCORD_USERNAME:-"All For One Notifier"}
-DISCORD_WEBHOOK_URL=https://discord.com/api/webhooks/776042303902973983/8N3sMKNc_3FD6xdpqXq0jT70O29QR3DeU_NA85V3DpvKEloK44pTs4MgBavHcM1ijhff
+DISCORD_WEBHOOK_URL=
 if [ -z "$DISCORD_WEBHOOK_URL" ]; then
      echo "You must define DISCORD_WEBHOOK_URL in order to post a message";
       exit 1;
       fi
-         curl -X POST --data-urlencode 'payload_json={"username": "'"$DISCORD_USERNAME"'", "content":"'"New Changes! - /google command changed to /image, new command /nasa added type the command to get help"'"}' $DISCORD_WEBHOOK_URL  
+         curl -X POST --data-urlencode 'payload_json={"username": "'"$DISCORD_USERNAME"'", "content":"'"TYPE HERE!"'"}' $DISCORD_WEBHOOK_URL  
