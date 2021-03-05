96b1cb730badf0272f593f83b43811be3635606f
Blaze <sandevabeykoon123@gmail.com>
2021-03-05 20:33:29 +0530

    removed(webhooks) folder removed with shell cmd

--- src/webhooks/notificationWebhook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DISCORD_USERNAME=${DISCORD_USERNAME:-"All For One Notifier"}
-DISCORD_WEBHOOK_URL=
-if [ -z "$DISCORD_WEBHOOK_URL" ]; then
-     echo "You must define DISCORD_WEBHOOK_URL in order to post a message";
-      exit 1;
-      fi
-         curl -X POST --data-urlencode 'payload_json={"username": "'"$DISCORD_USERNAME"'", "content":"'"TYPE HERE!"'"}' $DISCORD_WEBHOOK_URL  
