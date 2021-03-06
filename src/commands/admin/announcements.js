290ef57e7db75dfaff257de477439dbbb6f90178
Blaze <sandevabeykoon123@gmail.com>
2021-03-06 18:59:55 +0530

    update:(announcements.js) optimized env var with RegEx

--- src/commands/admin/announcements.js
+++ src/commands/admin/announcements.js
@@ -2,16 +2,19 @@ const Discord = require("discord.js");
 const webhook = require("webhook-discord");
 const { PREFIX } = require("../../../config.json");
 require("dotenv").config();
-const webhookID = process.env.WEBHOOK_ID;
-const webhookToken = process.env.WEBHOOK_TOKEN;
+var webhookUrl = process.env.ANNOUNCEMENTS_WEBHOOK_URL;
+webhookUrl += "/";
+const res = webhookUrl.match("https://.+/api/webhooks/([^/]+)/([^/]+)/");
 
 module.exports = {
   name: "announce",
   description: "Sends an announcement",
   async execute(message, args) {
+    const webhookID = res[1];
+    const webhookToken = res[2];
+
     if (!message.member.hasPermission("ADMINISTRATOR"))
       return message.reply("You do not have the permissions to do that");
-
     const webhookClient = new Discord.WebhookClient(webhookID, webhookToken);
     args = message.content.slice(9).trim();
     var announcement = "";
@@ -19,6 +22,5 @@ module.exports = {
       announcement = args;
     }
     webhookClient.send(announcement).catch(console.error);
-    message.author.send("**Announcement status: sent**");
   },
 };
