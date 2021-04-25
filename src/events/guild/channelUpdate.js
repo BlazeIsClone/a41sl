08aac5321a65970a339a34093aa2dfc397212c62
Blaze <sandevabeykoon123@gmail.com>
2021-04-25 20:45:16 +0530

    fix:(src/events/guild/channelUpdate) typo string try 2

--- src/events/guild/channelUpdate.js
+++ src/events/guild/channelUpdate.js
@@ -29,7 +29,7 @@ module.exports = async (client, newChannel, oldChannel) => {
               "** Channel Name\n\n**Old Name:** ``" +
               `${oldChannel.name}` +
               "\n**New Name:** ``" +
-              `${newChannel.name}``` +
+              `${newChannel.name}` +
               "\n**By:** <@" +
               `${userID}` +
               ">"
