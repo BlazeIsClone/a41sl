9e36d273dabe118b930e8a6d704eca578d0fbcc5
Blaze <sandevabeykoon123@gmail.com>
2021-08-19 09:06:19 +0530

    update:(./events/client/ready.js) add looping presence

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,15 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}!`);
-    console.log("Discord.js API Ready! ⚡");
+
+const arrayOfStatus = [`Welcome to ${client.guilds.cache.map((guild) => guild.name)}`, "Have Fun!", "Do Crazy Shit!", "Dont Kill ur self"]
+
+  console.log(`Logged in as ${client.user.username}!`);
+  console.log("Discord.js API Ready! ⚡");
+  let index = 0;
+
+  setInterval(() => {
+    if (index === arrayOfStatus.length) index = 0;
+    const status = arrayOfStatus[index];
     client.user.setPresence({
-        status: "online",
-        activity: {
-            name: "/help | 🧃",
-            type: "PLAYING",
-            details: null,
-            url: null,
-        },
+      status: "online",
+      activity: {
+        name: `${status}`,
+        type: "PLAYING",
+        details: null,
+        url: null,
+      },
     });
+    index++;
+  }, 5000);
 };
